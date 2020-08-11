4e9959e913123f54a2cad19cd3ebb33a8a03542a
afteralec <github email address>
2020-08-11 11:43:07 -0700

    Solution, first attempt

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,2 +1,18 @@
 class Episode < ApplicationRecord
+  has_many :appearances
+  has_many :guests, through: :appearances
+
+  def print_date
+    self.date.strftime('%B %d, %Y')
+  end
+
+  def print_num_and_date
+    "Episode #{self.number} - #{self.print_date}"
+  end
+
+  def average_rating
+    ratings = self.appearances.map(&:rating)
+
+    (ratings.sum.to_f / ratings.length).round(1)
+  end
 end
